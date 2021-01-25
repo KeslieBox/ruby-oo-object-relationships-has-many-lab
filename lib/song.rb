e42aceba206df47b08c56d90c2b195a47dfcc817
KeslieBox <kesliebox@gmail.com>
2021-01-25 17:30:58 -0600

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+attr_accessor :artist, :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def artist_name
+       @artist.name == @artist ? @artist.name : nil
+    end
+
+
+end
